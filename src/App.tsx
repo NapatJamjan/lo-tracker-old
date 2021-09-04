1394e4b10256923cd344ae5c42cb92a444cb585a
CrazyStormZ <earthhnapat@gmail.com>
2021-09-04 16:17:47 +0700

    Add simple ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,78 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Navbar } from 'react-bootstrap';
 import './App.css';
-//npm install --save react-bootstrap@next bootstrap@5.1.0
+import CourseMainBody from './view/CourseMain';
+import {BrowserRouter as Router,Switch,Route, Link} from 'react-router-dom';
+//npm install
+
+//Home Page and basic
 function App() {
   return (
     <div className="App">
-      Hello
+      <Header/>
+      <Nav/>
+      <body>
+        <TopBar/>
+
+
+      </body>
+
+    <Router>
+    <li>
+        <Link to="/">Home</Link>
+    </li>
+    <li>
+        <Link to="/Course">Course</Link>
+    </li>
+
+    <Switch>
+      <Route exact path="/">
+        <Body />
+      </Route>
+      <Route path="/course">
+        <CourseMainBody />
+      </Route>
+    </Switch>
+    </Router>
     </div>
   );
+}
+
+function Body(){
+  return (
+  <div>
+    <h3>Course</h3>
+    <h4>Semester 1/2020</h4>
+  </div>
+  )
+}
+
+function TopBar(){
+  return (
+  <div className="page-header header">
+    <h3 style={{float:"left",position:"absolute",left:10}}>Icon Here</h3>
+    <button style={{float:"right",position:"absolute",right:10}}>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png" width="30"></img>
+    </button>
+    <h3 style={{textAlign:"center"}}>Example Page Header</h3>
+    
+  </div>
+  )
+}
+
+function Nav(){
+  return <Navbar className="col-md-12 d-none d-md-block bg-light sidebar">
+            <div className="sidebar-sticky"></div>
+            
+            </Navbar>
+}
+
+function Header(){
+  return(
+    <head>
+      
+    </head>
+  )
 }
 
 export default App;
---
+++ src/view/CourseMain.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './CourseMain.css';
+
+function CourseMainBody(){
+    return (
+    <div>
+        <p>Course Main Body</p>
+    </div>
+    )
+}
+
+function H(){
+    return (
+        <div>
+            h
+        </div>
+    )
+}
+
+export default CourseMainBody;
