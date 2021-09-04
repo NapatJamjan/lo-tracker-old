22717464e2b687be831a47f378ecec5ce6ad63d4
CrazyStormZ <earthhnapat@gmail.com>
2021-09-04 15:58:21 +0700

    Update package

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+//npm install --save react-bootstrap@next bootstrap@5.1.0
 function App() {
   return (
     <div className="App">
