7c6ebb471cb3f1d4366d3aae66cf4577972c4064
Nuttawut503 <nuttawut.promsuk@gmail.com>
2021-09-14 17:34:24 +0700

    added homepage state

---
+++ src/shared/classroom.tsx
@@ -0,0 +1,51 @@
+import React, { createContext, useState } from 'react';
+
+export interface CourseDetail {
+  id: string;
+  program: string;
+  name: string;
+  semester: number;
+  year: number;
+};
+
+interface ClassroomContent {
+  programs: Array<string>;
+  courses: Array<CourseDetail>;
+  addProgram: (program: string) => void;
+  addCourse: (courseDetail: Omit<CourseDetail, 'id'>) => void
+}
+
+export const ClassroomContext = createContext<ClassroomContent>({
+  programs: [],
+  courses: [],
+  addProgram: () => {},
+  addCourse: () => {}
+});
+
+interface ClassroomState {
+  programs: Array<string>;
+  courses: Array<CourseDetail>;
+};
+
+export const ClassroomProvider: React.FC = ({children}) => {
+  const [classroom, setClassroom] = useState<ClassroomState>({programs: [], courses: []});
+  const { programs, courses } = classroom;
+  const addProgram = (program: string) => setClassroom({...classroom, programs: [...classroom.programs, program]});
+  const addCourse = ({program, name, semester, year }: Omit<CourseDetail, 'id'>) => {
+    setClassroom({
+      ...classroom,
+      courses: [...classroom.courses, {
+        id: `${classroom.courses.length + 1}`,
+        program,
+        name,
+        semester,
+        year
+      }]
+    })
+  };
+  return (
+    <ClassroomContext.Provider value={{programs, courses, addProgram, addCourse}}>
+      {children}
+    </ClassroomContext.Provider>
+  );
+};
